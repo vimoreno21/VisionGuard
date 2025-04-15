5f4ee394546a4284426938768ef0ad6cab323916
victoria <vicmoreno2102@gmail.com>
2025-04-15 20:12:29 +0000

    added new api_url

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -5,4 +5,5 @@ MATCH_THRESHOLD = 0.5  # Lower values = stricter matching
 
 load_dotenv()
 # API_URL=os.getenv("API_URL")
-API_URL = "http://localhost:8000"  # Update this URL
+# API_URL = "http://localhost:8000"  # Update this URL
+API_URL = "http://127.0.0.1:8000"  # Update this URL
